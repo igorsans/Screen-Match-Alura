7947fe738c9d55842515527e307423aa9f72660b
Igor Santos <igor.santos0@hotmail.com>
2024-04-02 18:34:12 -0300

    Alura Screen Match

---
+++ src/Principal.java
@@ -0,0 +1,30 @@
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("The Godfather");
+        meuFilme.setAnoDeLancamento(1991);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("total de avaliaçoes: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println("A média é: " + meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+
+    }
+}
